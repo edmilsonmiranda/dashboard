9ef9c8e8484728625b1901a495918f16a9f1c0bc
edmilson <edneto.mn@gmail.com>
2023-04-12 10:00:48 -0300

    atualizando

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,20 @@
+let lightMode = localStorage.getItem('lightMode');
+if(lightMode === 'true'){
+    modeSwitch.classList.toggle("active");
+    topo.classList.toggle("light");
+}
+
 modeSwitch.onclick  = (e) => {
     e.target.classList.toggle("active");
     topo.classList.toggle("light");
+    if(topo.classList.contains('light')){
+        localStorage.setItem('lightMode', true);
+    }else{
+        localStorage.setItem('lightMode', false);
+    }
+}
+
+let btnAbrir = document.querySelector(".abrir-lateral");
+btnAbrir.onclick = function(){
+    lateral.classList.add("active");
 }
